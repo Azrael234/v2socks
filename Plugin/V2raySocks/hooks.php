b26fa799cf291170a61b6a09172d23a52e8aabc5
Zzm317 <admin@loli.ren>
2018-05-25 15:05:57 +0800

    update

--- Plugin/V2raySocks/hooks.php
+++ Plugin/V2raySocks/hooks.php
@@ -1,16 +1,34 @@
 <?php
 use WHMCS\Database\Capsule;
-add_hook('AfterCronJob', 1, function() 
-{
-	$query = \WHMCS\Database\Capsule::table('tblproducts')->where('servertype', 'V2raySocks')->get();
-    $query2 = \WHMCS\Database\Capsule::table('tblhosting')->get();
-    $query3 = \WHMCS\Database\Capsule::table('tblservers')->where('type', 'V2raySocks')->get();
-	$products = V2RaySocks_QueryToArray($query);
-    $clients = V2RaySocks_QueryToArray($query2);
-    $servers = V2RaySocks_QueryToArray($query3);
-    $pids = V2RaySocks_prase_pid($products);
-    $pro = V2RaySocks_get_client_products_with_pids($clients,$pids,array('Active','Suspended'));
-    $pro = V2RaySocks_update_network($pro,$servers,V2RaySocks_prase_product_DB($products),$products);
+add_hook('AfterCronJob', 1, function(){
+	try {
+		$query = \WHMCS\Database\Capsule::table('tblproducts')->where('servertype', 'V2raySocks')->get();
+	    $query2 = \WHMCS\Database\Capsule::table('tblhosting')->get();
+	    $query3 = \WHMCS\Database\Capsule::table('tblservers')->where('type', 'V2raySocks')->get();
+		$products = V2RaySocks_QueryToArray($query);
+	    $clients = V2RaySocks_QueryToArray($query2);
+	    $servers = V2RaySocks_QueryToArray($query3);
+	    $pids = V2RaySocks_prase_pid($products);
+	    $pro = V2RaySocks_get_client_products_with_pids($clients,$pids,array('Active','Suspended'));
+	    $pro = V2RaySocks_update_network($pro,$servers,V2RaySocks_prase_product_DB($products),$products);
+	}catch (Exception $e){
+	}
+});
+
+add_hook('DailyCronJob', 1, function() {
+	try {
+		$query = \WHMCS\Database\Capsule::table('tblproducts')->where('servertype', 'V2raySocks')->get();
+	    $query2 = \WHMCS\Database\Capsule::table('tblhosting')->get();
+	    $query3 = \WHMCS\Database\Capsule::table('tblservers')->where('type', 'V2raySocks')->get();
+		$products = V2RaySocks_QueryToArray($query);
+	    $clients = V2RaySocks_QueryToArray($query2);
+	    $servers = V2RaySocks_QueryToArray($query3);
+	    $pids = V2RaySocks_prase_pid($products);
+	    $pro = V2RaySocks_get_client_products_with_pids($clients,$pids,array('Active','Suspended'));
+	    $rproducts = V2RaySocks_RebuildProductArray($products);
+	    $pro = V2RaySocks_CalcBandReset($pro,$rproducts,$servers);
+	}catch (Exception $e){
+	}
 });
 
 function V2RaySocks_QueryToArray($query){
@@ -21,6 +39,14 @@ function V2RaySocks_QueryToArray($query){
             $producta[$k] = $produc;
         }
         $products[] = $producta;
+    }
+    return $products;
+}
+
+function V2RaySocks_RebuildProductArray($query){
+    $products = array();
+    foreach ($query as $product) {
+        $products[$product['id']] = $product;
     }
     return $products;
 }
@@ -80,5 +106,73 @@ function V2RaySocks_prase_product_DB($products,$module = 'V2raySocks'){
         }
     }
     return $product;
+}
+
+function V2RaySocks_CalcBandReset($pro,$products,$server){
+	foreach($server as $ser){
+        $mysql = new mysqli($ser['ipaddress'], $ser['username'], decrypt($ser['password']));
+        $servername = 'mysqlserver'.$ser['id'];
+        $$servername = $mysql;
+    }
+    $product = array();
+	foreach($pro as $por){	
+        $sid = $por['server'];
+        $mysql = 'mysqlserver'.$sid;
+        $sql = $$mysql;
+        $sql->select_db($products[$por['packageid']]['configoption1']);
+		$days = V2RaySocks_daysInmonth(date('y'),date('m'));
+		V2RaySocks_calcreset($por,$products[$por['packageid']],$days,$sql);
+    }
+}
+
+function V2RaySocks_resetband($id,$sqlserver){
+    $sqlserver->query("UPDATE `user` SET `u` = '0', `d` = '0' where `sid` = ".$id);
+    $sqlserver->query("DELETE from `user_usage` WHERE `sid` = ".$id);
+    echo("ID:".$id." Has been reset</br>");
+}
+
+function V2RaySocks_daysInmonth($year='',$month=''){  
+    if(empty($year)) $year = date('Y');  
+    if(empty($month)) $month = date('m');  
+    if (in_array($month, array(1, 3, 5, 7, 8, '01', '03', '05', '07', '08', 10, 12))) {    
+            $text = '31';        //月大  
+    }elseif ($month == 2 || $month == '02'){    
+        if ( ($year % 400 == 0) || ( ($year % 4 == 0) && ($year % 100 !== 0) ) ) {   //判断是否是闰年    
+            $text = '29';        //闰年2月  
+        } else {    
+            $text = '28';        //平年2月  
+        }    
+    } else {    
+        $text = '30';            //月小  
+    }  
+      
+    return $text;  
+}  
+
+function V2RaySocks_calcreset($product,$whmcs,$day,$sqlserver){
+    switch($whmcs['configoption2']){
+        case 0:
+            break;
+        case 1:
+            if(date("d", strtotime($product['nextduedate'])) == date('d')){
+                V2RaySocks_resetband($product['id'],$sqlserver);
+            }
+            if(date('d') == $day){
+                if(date("d", strtotime($product['nextduedate'])) > $day){
+                    V2RaySocks_resetband($product['id'],$sqlserver);
+                } 
+            }
+            break;
+        case 2:
+            if(date('d') == 1){
+                V2RaySocks_resetband($product['id'],$sqlserver);
+            }
+            break;
+        case 3:
+            if(date('d') == $day){
+                V2RaySocks_resetband($product['id'],$sqlserver);
+            }
+            break;
+    }
 }
 ?>
